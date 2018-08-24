55479173bdc63e2e8570cb54bec04d9406ce8205
asia <asiaelyse@gmail.com>
2018-08-24 06:07:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers, hundred grand, kitkat, skittles']
+
+function addElementToBeginningOfArray(chocolateBars,newel){
+  newel = 'gummy'
+  chocolateBars = newel...chocolateBars
+  return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
